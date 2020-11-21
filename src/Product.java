5a8b515fa5eb0ce4ec4b43746101a94eb20ad20b
Blake Del Rey <bcdelrey@gmail.com>
2020-11-21 15:33:28 -0800

    Started Working on Product Class
    Starting working on some of the base  attributes  and functions  for the product class

--- src/Product.java
+++ src/Product.java
@@ -1,13 +1,67 @@
 /*
  * class for containing the information of a product
  */
-public class Product 
+public class Product
 {
+	/**
+	* Name of the item
+	*/
+	private String name;
+
+	/**
+	* Price that the item sells for
+	*/
+	private double sellPrice;
+
+	/**
+	* Cost to attain 1 unit of this item before retail sale
+	*/
+	private double costPrice;
+
+
 	/*
-	 * Constructor
+	 * Default Constructor
 	 */
 	public Product()
 	{
-		//Initial Variables
+		name = "";
+		sellPrice = 0;
+		costPrice = 0;
 	}
+
+	/**
+	* Specific Constructor with values for all data members
+	*
+	* @param newName Initail name of item
+	* @param newSell Initial sell  price of item
+	* @param newCost Initial costprice of item
+	*/
+	public Product(String newName, double newSell, double newCost)
+	{
+		name = newName;
+		sellPrice = newSell;
+		costPrice = newCost;
+	}
+
+	/**
+	* Returns name of item
+	*
+	* @return Name of the item
+	*/
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	* Returns sell  price of item
+	*
+	* @return Sell price of the item
+	*/
+	public double getSellPrice()
+	{
+		return sellPrice;
+	}
+
+
 }
