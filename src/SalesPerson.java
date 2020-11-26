dd590c33e3b5b4c45c2e73d2150ea3c0f5cf6ca6
Britney Khuu <britneykhuu@gmail.com>
2020-11-25 17:12:32 -0800

    Worked on SalesPerson's basic code

--- src/SalesPerson.java
+++ src/SalesPerson.java
@@ -3,11 +3,74 @@
  */
 public class SalesPerson 
 {
+	private String firstName;
+	private String lastName;
+	private double commPercent;
 	/*
 	 * Constructor
 	 */
 	public SalesPerson()
 	{
 		//Initial Variables
+		firstName ="";
+		lastName = "";
+		commPercent = 0.0;
 	}
+	/**
+	 * Method which sets the first name of the SalesPerson
+	 * @param first
+	 */
+	public void setFirstName(String first){
+		firstName = first;
+	}
+	/**
+	 * Method which sets the last name of the SalesPerson
+	 * @param last
+	 */
+	public void setLastName(String last) {
+		lastName = last;
+	}
+	/**
+	 * Method that sets the Commission Percentage that the SalesPerson gets for their commission
+	 * @param percent
+	 */
+	public void setCommissionPercentage(double percent) {
+		commPercent = percent;
+	}
+	/**
+	 * Method which returns the SalesPerson's first name
+	 * @return first name of SalesPerson
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * Method which returns the SalesPerson's last name
+	 * @return last name of SalesPerson
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * Method which returns the commission percentage that is applies for the SalesPerson
+	 * @return commission percentage
+	 */
+	public double getCommissionPercentage() {
+		return commPercent;
+	}
+	/**
+	 * Method which returns the total Commissions that the SalesPerson has done
+	 * @return total commissions done
+	 */
+	public int getTotalCommissions() {
+		return 0;
+	}
+	/**
+	 * Method which returns the payment to the SalesPerson of their total sales
+	 * @return payment to the SalesPerson of their commissions and the applied Commission percentage
+	 */
+	public double getPayment() {
+		return 0.0;
+	}
+	
 }
