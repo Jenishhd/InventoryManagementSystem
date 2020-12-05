750a57c5ac5a05b5bc5fe26e84b64f4ec780204b
Blake Del Rey <bcdelrey@gmail.com>
2020-12-05 14:03:06 -0800

    Allow For Repeated Warehouse menu
    Warehouse menu now loops properly

--- src/WarehouseMenuBoundary.java
+++ src/WarehouseMenuBoundary.java
@@ -18,57 +18,65 @@ public class WarehouseMenuBoundary
      /*
       * Method to print all menu choices and act upon the input of the user
       */
-     public boolean printWarehouseMenuChoices()
+     public void printWarehouseMenuChoices()
      {
           Scanner input = new Scanner(System.in);
-          System.out.println("\nPlease make a selection:");
-          System.out.println("1) Add Warehouse\n2) Remove Warehouse\n3) Show all warehouses\n4) Display All Products With Quantity \n5) Remove item from warehouse\n6) Supply item in warehouse\n7) Remove item supply in warehouse\n8) Exit");
-          //Switch for menu selection. All cases that return true will reprint the menu choices. All cases that return false will not
-          switch(input.nextLine())
+          String selection = "0";
+          while (!selection.equals("8"))
           {
-          //Add customer
-          case "1":
-               warehouseController.addWarehouse();
-               return true;
-          //Remove customer
-          case "2":
-               warehouseController.removeWarehouse();
-               return true;
-          //Show all customers
-          case "3":
-               warehouseController.displayWarehouses();
-               return true;
-          //Adds a new item to the warehouse. Item must already exist in ItemMenuBoundary
-          case "4":
-               ///Asks user to select an item in the itemMenuBoundary to add, and the quantity
-               warehouseController.displayAllWarehousesWithProducts();
-               return true;
-          //Removes item entirely from warehouse. Item must already be contained in warehouse
-          case "5":
-               ///Asks user to select an item in the warehouse to remove
-               warehouseController.removeWarehouseItem();
-               return true;
-          //Adds a certain amount to an item in the warehouse. Item must exist in the warehouse already
-          case "6":
-               System.out.print("Enter name of warehouse you'd like to edit product quantity: ");
-               String warehouseToEdit = input.nextLine();
-               System.out.print("Enter name of product you'd like to edit quantity of: ");
-               String productToEdit = input.nextLine();
-               System.out.print("Enter new quantity for product: ");
-               int newQuantity = input.nextInt();
-               warehouseController.changeWarehouseItemQuantity(warehouseToEdit, productToEdit, newQuantity);
-               return true;
-          //Removes a certain amount of an item form the warehouse. Item must exist in the warehouse already, and resulting item quantity cannot be <0
-          case "7":
-               ///Asks user for number to remove
-               //warehouseController.changeWarehouseItemQuantity(/*negative int*/);
-               return true;
-          //Exit
-          case "8":
-               return false;
-          default:
-               System.out.println("Invalid selection. Please try again.");
-               return true;
+               System.out.println("\nPlease make a selection:");
+               System.out.println("1) Add Warehouse\n2) Remove Warehouse\n" +
+                                  "3) Show all warehouses\n4) Display All Products With Quantity \n" +
+                                  "5) Edit quantity of an item\n6) Supply item in warehouse\n" +
+                                  "7) Remove item supply in warehouse\n8) Exit");
+               selection = input.nextLine();
+               //Switch for menu selection. All cases that return true will reprint the menu choices. All cases that return false will not
+               switch(selection)
+               {
+               //Add customer
+               case "1":
+                    warehouseController.addWarehouse();
+                    break;
+               //Remove customer
+               case "2":
+                    warehouseController.removeWarehouse();
+                    break;
+               //Show all customers
+               case "3":
+                    warehouseController.displayWarehouses();
+                    break;
+               //Adds a new item to the warehouse. Item must already exist in ItemMenuBoundary
+               case "4":
+                    ///Asks user to select an item in the itemMenuBoundary to add, and the quantity
+                    warehouseController.displayAllWarehousesWithProducts();
+                    break;
+               //Allows user to edit quantity of an item in a warehouse
+               case "5":
+                    ///Asks user to select an item in the warehouse to remove
+                    System.out.print("Enter name of warehouse you'd like to edit product quantity: ");
+                    String warehouseToEdit = input.nextLine();
+                    System.out.print("Enter name of product you'd like to edit quantity of: ");
+                    String productToEdit = input.nextLine();
+                    System.out.print("Enter new quantity for product: ");
+                    int newQuantity = input.nextInt();
+                    warehouseController.changeWarehouseItemQuantity(warehouseToEdit, productToEdit, newQuantity);
+                    break;
+               //Adds a certain amount to an item in the warehouse. Item must exist in the warehouse already
+               case "6":
+                    break;
+               //Removes a certain amount of an item form the warehouse. Item must exist in the warehouse already, and resulting item quantity cannot be <0
+               case "7":
+                    ///Asks user for number to remove
+                    //warehouseController.changeWarehouseItemQuantity(/*negative int*/);
+                    break;
+               //Exit
+               case "8":
+                    break;
+               default:
+                    System.out.println("Invalid selection. Please try again.");
+                    break;
+               }
           }
+
      }
 }
