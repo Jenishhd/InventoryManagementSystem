9db28ccb0bce940a9034042097615e0b2ca0c869
Blake Del Rey <bcdelrey@gmail.com>
2020-12-06 15:08:39 -0800

    Got Rid Of Remove Product Because it says we dont need it in RFP

--- src/Boundary/ProductMenuBoundary.java
+++ src/Boundary/ProductMenuBoundary.java
@@ -18,10 +18,10 @@ public class ProductMenuBoundary
 	{
           Scanner input = new Scanner(System.in);
           int selection = 0;
-          while (selection != 4)
+          while (selection != 3)
           {
                System.out.println("\nPlease make a selection:");
-     		System.out.println("1) Add Item\n2) Remove Item\n3) Show all items\n4) Exit");
+     		System.out.println("1) Add Item\n2) Show all items\n3) Exit");
                selection = input.nextInt();
      		//Switch for menu selection. All cases that return true will reprint the menu choices. All cases that return false will not
      		switch(selection)
@@ -30,16 +30,12 @@ public class ProductMenuBoundary
      		case 1:
      			productController.addProduct();
      			break;
-     		//Remove item
-     		case 2:
-     			productController.removeProduct();
-     			break;
      		//Show all items
-     		case 3:
+     		case 2:
      			productController.displayProducts();
                     break;
      		//Exit
-     		case 4:
+     		case 3:
      			break;
      		default:
      			System.out.println("Invalid selection. Please try again.");
